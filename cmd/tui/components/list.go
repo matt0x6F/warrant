ebfb91b9f4a8dcccf3c582da65092215147c68b8
Matt Ouille <matt.ouille@protonmail.com>
2026-03-13 21:18:23 -0700

    tui: implement SelectList.Render

--- cmd/tui/components/list.go
+++ cmd/tui/components/list.go
@@ -1,5 +1,7 @@
 package components
 
+import "strings"
+
 type SelectList struct {
 	Items        []string
 	Selected     int
@@ -10,6 +12,13 @@ func (s SelectList) Render(width int) string {
 	if len(s.Items) == 0 {
 		return Muted.Render(s.EmptyMessage)
 	}
-	// TODO: implement in next task
-	return ""
+	var lines []string
+	for i, item := range s.Items {
+		if i == s.Selected {
+			lines = append(lines, Primary.Render("▸ ")+Secondary.Render(item))
+		} else {
+			lines = append(lines, "  "+Secondary.Render(item))
+		}
+	}
+	return strings.Join(lines, "\n")
 }
